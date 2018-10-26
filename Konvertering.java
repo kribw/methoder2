d0d12f11a2334e2befda86f030dd5a8220110d4f
kribw <44354033+kribw@users.noreply.github.com>
2018-10-26 23:11:53 +0200

    Update Konvertering.java

--- Konvertering.java
+++ Konvertering.java
@@ -1,24 +1,19 @@
 public class Konvertering {
-    public static double fahrenheitStart = 120;
     public static void main(String[] args) {
+        double fahrenheitStart = 120;
         System.out.println("Celsius\t\tFahrenheit\t|\tFahrenheit\tCelsius");
         System.out.println("--------------------------------------------------");
-
         for(double celsiusStart = 40; celsiusStart > 30; celsiusStart--) {
-            double konvertertFahrenheit = celsiusToFahrenheit(celsiusStart);
-            double konvertertCelsius = fahrenheitToCelsius(fahrenheitStart);
-            System.out.printf("%.2f\t\t%.2f\t\t|\t%.2f\t\t%.2f\n", celsiusStart, konvertertFahrenheit, fahrenheitStart, konvertertCelsius);
+            System.out.printf("%.2f\t\t%.2f\t\t|\t%.2f\t\t%.2f\n", celsiusStart, celsiusToFahrenheit(celsiusStart), fahrenheitStart, fahrenheitToCelsius(fahrenheitStart));
             fahrenheitStart -= 10;
         }
     }
 
     public static double celsiusToFahrenheit(double celsius) {
-        double fahrenheitConv = (9.0 / 5) * celsius + 32;
-        return fahrenheitConv;
+        return (9.0 / 5) * celsius + 32;
     }
 
     public static double fahrenheitToCelsius(double fahrenheit) {
-        double celsiusConv = (5.0 / 9) * (fahrenheit - 32);
-        return celsiusConv;
+        return (5.0 / 9) * (fahrenheit - 32);
     }
 }
